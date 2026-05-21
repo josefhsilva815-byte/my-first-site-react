aec42c7a0977cc8e5dc1404f0b79d5d1a388ff99
josefhsilva815-byte <josefhsilva815@gmail.com>
2026-05-21 00:01:27 -0300

    Centralizando texto dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,12 @@ function App() {
     borda: `
       border border-black
     `,
+    botao: `
+      text-center place-content-center
+    `,
     
     
-    testStyle:(<o className="text-right justify-items-start"/>)
+    testStyle:(<o className="flex text-center size-full "/>)
   }
 
 
@@ -27,27 +30,27 @@ function App() {
         <Input tp="text" ph="Digite" cls={`flex h-full text-left p-2 col-start-1 col-end-5 row-start-1 ${stl.borda}`}/>
         <Output tp="text" ph="Resultado" cls={`self-end pr-2 pb-2 text-end h-6 bg-transparent col-start-1 col-end-5 row-start-1 outline-none`}/>
 
-        <Botao btn="1" cls={`${stl.borda}`}/>
-        <Botao btn="2" cls={`${stl.borda}`}/>
-        <Botao btn="3" cls={`${stl.borda}`}/>
-        <Botao btn="4" cls={`${stl.borda} col-start-1 row-start-4`}/>
-        <Botao btn="5" cls={`${stl.borda} col-start-2 row-start-4`}/>
-        <Botao btn="6" cls={`${stl.borda} col-start-3 row-start-4`}/>
-        <Botao btn="7" cls={`${stl.borda} col-start-1 row-start-5`}/>
-        <Botao btn="8" cls={`${stl.borda} col-start-2 row-start-5`}/>
-        <Botao btn="9" cls={`${stl.borda} col-start-3 row-start-5`}/>
-        <Botao btn="0" cls={`${stl.borda} col-start-2 row-start-6`}/>
+        <Botao btn="1" cls={`${stl.botao} ${stl.borda}`}/>
+        <Botao btn="2" cls={`${stl.botao} ${stl.borda}`}/>
+        <Botao btn="3" cls={`${stl.botao} ${stl.borda}`}/>
+        <Botao btn="4" cls={`${stl.botao} ${stl.borda} col-start-1 row-start-4`}/>
+        <Botao btn="5" cls={`${stl.botao} ${stl.borda} col-start-2 row-start-4`}/>
+        <Botao btn="6" cls={`${stl.botao} ${stl.borda} col-start-3 row-start-4`}/>
+        <Botao btn="7" cls={`${stl.botao} ${stl.borda} col-start-1 row-start-5`}/>
+        <Botao btn="8" cls={`${stl.botao} ${stl.borda} col-start-2 row-start-5`}/>
+        <Botao btn="9" cls={`${stl.botao} ${stl.borda} col-start-3 row-start-5`}/>
+        <Botao btn="0" cls={`${stl.botao} ${stl.borda} col-start-2 row-start-6`}/>
 
-        <Botao btn="+" cls={`${stl.borda} col-start-2 row-start-2`}/>
-        <Botao btn="-" cls={`${stl.borda} col-start-3 row-start-2`}/>
-        <Botao btn="/" cls={`${stl.borda} col-start-4 row-start-3`}/>
-        <Botao btn="x" cls={`${stl.borda} col-start-4 row-start-4`}/>
-        <Botao btn="." cls={`${stl.borda} col-start-1 row-start-6`}/>
-        <Botao btn="%" cls={`${stl.borda} col-start-3 row-start-6`}/>
+        <Botao btn="+" cls={`${stl.botao} ${stl.borda} col-start-2 row-start-2`}/>
+        <Botao btn="-" cls={`${stl.botao} ${stl.borda} col-start-3 row-start-2`}/>
+        <Botao btn="/" cls={`${stl.botao} ${stl.borda} col-start-4 row-start-3`}/>
+        <Botao btn="x" cls={`${stl.botao} ${stl.borda} col-start-4 row-start-4`}/>
+        <Botao btn="." cls={`${stl.botao} ${stl.borda} col-start-1 row-start-6`}/>
+        <Botao btn="%" cls={`${stl.botao} ${stl.borda} col-start-3 row-start-6`}/>
 
-        <Botao btn="Del" cls={`${stl.borda} col-start-4 row-start-2`}/>
-        <Botao btn="C"   cls={`${stl.borda} col-start-1 row-start-2`}/>
-        <Botao btn="="   cls={`${stl.borda} col-start-4 row-start-5 row-end-7`}/>
+        <Botao btn="Del" cls={`${stl.botao} ${stl.borda} col-start-4 row-start-2`}/>
+        <Botao btn="C"   cls={`${stl.botao} ${stl.borda} col-start-1 row-start-2`}/>
+        <Botao btn="="   cls={`${stl.botao} ${stl.borda} col-start-4 row-start-5 row-end-7`}/>
       </div>
     </div>
   )
